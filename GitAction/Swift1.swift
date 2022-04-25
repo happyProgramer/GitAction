711030d54db5bb30c6cc0b31e5d61b88d090f9d4
happyProgramer <15801067062@163.com>
2022-04-25 15:32:37 +0800

    Add Swift1

---
+++ GitAction/Swift1.swift
@@ -0,0 +1,20 @@
+//
+//  Swift1.swift
+//  GitAction
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+struct Swift1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Swift1_Previews: PreviewProvider {
+    static var previews: some View {
+        Swift1()
+    }
+}
