2640a451cefee2f664a0346d2367559b9f40fa8c
happyProgramer <15801067062@163.com>
2022-04-25 14:56:51 +0800

    Create repository

---
+++ GitAction/GitActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitActionApp.swift
+//  GitAction
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GitActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
