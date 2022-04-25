548b90b4fd6940ce1e30278cbd3994a72b6a5855
happyProgramer <15801067062@163.com>
2022-04-25 15:33:32 +0800

    Add Swift3

---
+++ GitAction/Swift3.swift
@@ -0,0 +1,20 @@
+//
+//  Swift3.swift
+//  GitAction
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+struct Swift3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Swift3_Previews: PreviewProvider {
+    static var previews: some View {
+        Swift3()
+    }
+}
