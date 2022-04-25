2f971e274bcfd2d26ea4eae261fb73ba2dbb6b0f
happyProgramer <15801067062@163.com>
2022-04-25 15:33:05 +0800

    Add Swift2

---
+++ GitAction/Swift2.swift
@@ -0,0 +1,20 @@
+//
+//  Swift2.swift
+//  GitAction
+//
+//  Created by yanping han on 2022/4/25.
+//
+
+import SwiftUI
+
+struct Swift2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Swift2_Previews: PreviewProvider {
+    static var previews: some View {
+        Swift2()
+    }
+}
